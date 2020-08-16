5ccd0c8ba86e1460211a602b5aed90823945e8a2
otherMax <45923941+otherMax@users.noreply.github.com>
2020-08-16 17:36:48 -0500

    Add files via upload

--- includes/footer.js
+++ includes/footer.js
@@ -1,2 +1,2 @@
 /*footer*/
-document.write('<div class="footer container-fluid text-center py-2 bg-dark text-white-50"><span>Rasby Media 2020</div>');
+document.write('<div class="footer container-fluid text-center py-2 text-white-50" style=" background-color: black; font-size: 9px"><span>THIS SITE IS NOT AFFILIATED WITH VALVE SOFTWARE, BLUEHOLE OR DAYBREAK GAME COMPANY. RASBY MEDIA 2020</div>');
