94e900407858b13385798e35b724503a228fe086
otherMax <45923941+otherMax@users.noreply.github.com>
2021-03-16 12:22:56 -0500

    Delete includes directory

--- includes/footer.js
+++
@@ -1,2 +0,0 @@
-/*footer*/
-document.write('<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><div class="footer container-fluid text-center py-2 text-white-50" style=" background-color: black; font-size: 9px"><span>THIS SITE IS NOT AFFILIATED WITH VALVE SOFTWARE, BLUEHOLE OR DAYBREAK GAME COMPANY. RASBY MEDIA 2020</div>');
