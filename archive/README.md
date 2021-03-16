8d22eeeeb860c20d073f85951aff789cd5940e66
otherMax <45923941+otherMax@users.noreply.github.com>
2021-03-16 12:24:53 -0500

    Add files via upload

---
+++ archive/README.md
@@ -0,0 +1,2 @@
+# v2
+new website. Home of the RASBYS
---
+++ archive/v2/includes/footer.js
@@ -0,0 +1,2 @@
+/*footer*/
+document.write('<div class="footer container-fluid text-center py-2 text-white-50" style=" background-color: black; font-size: 9px"><span>THIS SITE IS NOT AFFILIATED WITH VALVE SOFTWARE, BLUEHOLE OR DAYBREAK GAME COMPANY. RASBY MEDIA 2020</div>');
